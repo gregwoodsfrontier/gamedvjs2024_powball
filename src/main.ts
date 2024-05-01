5ece6f5fc0fd61491777732601d3c34040e8cfc0
Richard Chung <iwannaplaygamesjp@gmail.com>
2024-05-01 21:19:59 +0000

    first commit to testing branch

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { UIScene } from './scenes/UI';
 import { GameContainer } from './scenes/GameContainer';
 import { MainMenu } from './scenes/MainMenu';
 import { Preload } from './scenes/Preload'
+import { Test } from './scenes/Test';
+import { PhaserPlanck } from "phaser-planck";
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
@@ -20,8 +22,22 @@ const config: Types.Core.GameConfig = {
         mode: Scale.FIT,
         autoCenter: Scale.CENTER_BOTH
     },
+    plugins: {
+        scene: [{ key: "PhaserPlanck", plugin: PhaserPlanck, mapping: "planck" }],
+    },
+    physics: {
+        planck: {
+            debug: true,
+            scaleFactor: 30,
+            gravity: {
+                x: 0,
+                y: 3
+            }
+        }
+    },
     scene: [
         Preload,
+        Test,
         GameContainer,
         
         MainMenu,
